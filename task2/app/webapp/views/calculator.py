c3c4bfc7f0233eeb73654f1b6f113cf206284601
Nickname Lastname <adada2231wadsa324@gmail.com>
2023-04-14 18:53:04 +0600

    Методы для двух заданий, которые формируют действия при операции с числами

---
+++ task2/app/webapp/views/calculator.py
@@ -0,0 +1,73 @@
+import json
+from django.http import HttpResponse, HttpResponseNotAllowed, JsonResponse
+from django.shortcuts import render
+from django.views import View
+from django.views.decorators.csrf import ensure_csrf_cookie
+
+
+class IndexView(View):
+    def get(self, request):
+        return render(request, 'index.html')
+
+
+def add(request, *args, **kwargs):
+    numbers = json.loads(request.body)
+    try:
+        answer = sum(numbers.values())
+        answer_as_json = json.dumps({"answer": answer})
+        response = HttpResponse(answer_as_json, content_type='application/json')
+        response.status_code = 201
+    except Exception:
+        response_data = {'detail': "Некорректный ввод!"}
+        response = JsonResponse(response_data)
+        response.status_code = 400
+    return response
+
+
+def subtract(request, *args, **kwargs):
+    numbers = json.loads(request.body)
+    try:
+        answer = numbers["A"] - numbers["B"]
+        answer_as_json = json.dumps({"answer": answer})
+        response = HttpResponse(answer_as_json, content_type='application/json')
+        response.status_code = 201
+    except Exception:
+        response_data = {'detail': "Некорректный ввод!"}
+        response = JsonResponse(response_data)
+        response.status_code = 400
+    return response
+
+
+def multiply(request, *args, **kwargs):
+    numbers = json.loads(request.body)
+    try:
+        answer = numbers["A"] * numbers["B"]
+        answer_as_json = json.dumps({"answer": answer})
+        response = HttpResponse(answer_as_json, content_type='application/json')
+        response.status_code = 201
+    except Exception:
+        response_data = {'detail': "Некорректный ввод!"}
+        response = JsonResponse(response_data)
+        response.status_code = 400
+    return response
+
+
+def divide(request, *args, **kwargs):
+    numbers = json.loads(request.body)
+    try:
+        answer = numbers["A"] / numbers["B"]
+        answer_as_json = json.dumps({"answer": answer})
+        response = HttpResponse(answer_as_json, content_type='application/json')
+        response.status_code = 201
+    except Exception:
+        response_data = {'detail': "Некорректный ввод!"}
+        response = JsonResponse(response_data)
+        response.status_code = 400
+    return response
+
+
+@ensure_csrf_cookie
+def get_token_view(request, *args, **kwargs):
+    if request.method == "GET":
+        return HttpResponse()
+    return HttpResponseNotAllowed(f'Only GET request are allowed {request.method}')
